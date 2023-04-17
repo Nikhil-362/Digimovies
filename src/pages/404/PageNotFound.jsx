db033cd4a75753e81b592366280b9cbdb8dae293
Nikhil-362 <reddynikhil757@gmail.com>
2023-04-17 12:24:23 +0530

    Completed

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import styled from "styled-components"
+import ContentWrapper from '../../components/contentWrapper/ContentWrapper'
 
 function PageNotFound() {
   return (
     <div>
-      <h1>Page Not Found</h1>
+      <Pnf>
+        <ContentWrapper>
+        <h1>404</h1>
+        <p>Page not found!</p>
+        </ContentWrapper>
+      </Pnf>
     </div>
   );
 }
 
 export default PageNotFound;
+const Pnf = styled.div`
+    text-align: center;
+    color: red;
+    height: 700px;
+    padding-top: 200px;
+
+    h1{
+            font-size: 150px;
+            font-weight: 700;
+        }
+    p{
+            font-size: 44px;
+        }
+`
