98df5578c5a9dc224d3cfe257acdf7001483df8e
R.Nikhil Reddy <122670124+Nikhil-362@users.noreply.github.com>
2023-04-19 13:16:53 +0530

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -19,9 +19,9 @@ const Home = () => {
       <Back>      
           <Trending/>
           <TopRated/>
-          <IndianMovies/>
-//           <IndianMovies2/>
-//           <IndianMovies3/>
+    {/* <IndianMovies/>
+            <IndianMovies2/>
+             <IndianMovies3/> */}
           <Popular/>
         
        
