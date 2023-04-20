9a911b5dcce6e4e463f7ce92addb35745ef61770
R.Nikhil Reddy <122670124+Nikhil-362@users.noreply.github.com>
2023-04-20 13:51:21 +0530

    Update IndianMovies.jsx

--- src/pages/home/Indian Movies/IndianMovies.jsx
+++ src/pages/home/Indian Movies/IndianMovies.jsx
@@ -1,38 +1,36 @@
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
 import useFetch from "../../../hooks/useFetch";
 import './IndianMovies.scss'
 import Carousel from '../../../components/carousel/Carousel';
 import SwitchTabs from '../../../components/switchTabs/SwitchTabs';
 
-
-
 const IndianMovies = () => {
   
- const [endpoint, setEndpoint] = useState("te");
+  const [endpoint, setEndpoint] = useState("te");
+  const [filters, setFilters] = useState({ with_original_language: "te", region: "in" });
 
- let filters ={ with_original_language: endpoint, region: "in"}
- const { data, loading } = useFetch("/discover/movie/", filters , endpoint);
+  const { data, loading } = useFetch("/discover/movie/", filters);
 
-  const onTabChange =(tab)=>{
- setEndpoint(tab === "Telugu"? "te" : "hi");
+  useEffect(() => {
+    setFilters({ with_original_language: endpoint, region: "in" });
+  }, [endpoint]);
 
-console.log(endpoint);
+  const onTabChange = (tab) => {
+    setEndpoint(tab === "Telugu" ? "te" : "hi");
   }
 
   return (
     <div className='carouselSection'>
-        <ContentWrapper>
-          <div className="space">
-        <span className="carouselTitle">
-           Indian Movies
-        </span>
-       
-        <SwitchTabs data={["Telugu","Hindi"]} onTabChange={onTabChange}/>
-        
+      <ContentWrapper>
+        <div className="space">
+          <span className="carouselTitle">
+            Indian Movies
+          </span>
+          <SwitchTabs data={["Telugu","Hindi"]} onTabChange={onTabChange}/>
         </div>
-        </ContentWrapper>
-        <Carousel data={data?.results} loading={loading}/>
+      </ContentWrapper>
+      <Carousel data={data?.results} loading={loading}/>
     </div>
   );
 }
